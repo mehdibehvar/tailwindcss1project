c7e8cbebc851fa9de986af30fa50728b6d80387b
mehdibehvar <mehdibehvar00@gmail.com>
2023-04-01 23:03:06 +0430

    initial-commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mcc: {
+          white: "#ffffff",
+          white50: "rgba(255, 255, 255, 0.5)",
+          yellow50: "#FFB000", ////primary-50
+          yellow75: "#CE9414", ////primary-75
+          gray75: "#62615e", ///gray-75
+          gray50: "#BABABA", ///gray-50
+          gray25: "#838282", ///gray-25
+          gray20: "rgba(151, 151, 151, 0.195)", ///gray-25
+          info: "#0090B4",
+          violet: "#B9A7FF",
+          pink: "#FF89AE",
+          black: "#000000",
+        },
+      },
+      fontFamily: {
+        'doran': ["doran"],
+        'yekan': ['yekan'],
+        'lalezar':["lalezar"],
+        'vazir':['Vazir'],
+        'PelakFA':['PelakFA'],
+        'IRANYekan':['IRANYekanXFaNum'],
+      },
+      backgroundImage: {
+        'hero-header': "url('../src/assets/images/herobg.png')",
+        'brand-logo':"url('../src/assets/images/Logo.png')",
+        'mask-group':"url('../src/assets/images/Mask group.png')",
+        'last-article':"url('../src/assets/images/lastarticls.png')"
+      }
+    },
+  },
+  plugins: [],
+};
